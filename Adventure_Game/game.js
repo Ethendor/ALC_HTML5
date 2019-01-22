fe49034a01eb65e16ac730cc01343b123771fc6d
Darien <darien.legates@gmail.com>
2019-01-22 10:29:18 -0700

    Got started on Text Adventure

---
+++ Adventure_Game/game.js
@@ -0,0 +1,18 @@
+// Commenting, Yay!
+/*
+Commenting
+On
+Multiple
+Lines,
+Yay!
+*/
+
+// alert("Warning will self destruct!")
+
+// confirm("Do you want to die?")
+
+// prompt("What type of death would you prefer")
+
+var playerName = prompt("What is your name !")
+
+document.write(playerName)
