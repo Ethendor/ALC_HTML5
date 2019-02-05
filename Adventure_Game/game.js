b444e4752c46ff0bf9ff3dc957c14ca42d3d86cc
Darien <darien.legates@gmail.com>
2019-02-05 10:29:30 -0700

    Added Inventory

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -17,6 +17,16 @@ Yay!
 
 // document.write(playerName)
 
+
+// declare inventory
+var inventory = {
+    coins: 0,
+    sword: 0,
+    shield: 0,
+}
+
+
+
 Game();
 
 function Game(){
@@ -41,7 +51,7 @@ function Game(){
     City();
     
     function City(){
-        var city = prompt("Well " + jeffName + " you're here in the city of Taliman. One of the richest and most beatiful cities in Cloudborg. Filled with huge shops and villages and a strong millitary that houses inside the castle. Here you see many places that to explore. You can go to the Inn where many adventurers find their purpose and spirit. The Castle also has a great training grounds and equipment for new adventurers. There's also some exciting Cloud Mountains to the left. Or you can travel to the ground. \n -Go to Inn \n -Go to Castle \n -Go to the Mountains \n -Travel to the ground");
+        var city = prompt("Well " + jeffName + " you're here in the city of Taliman. One of the richest and most beatiful cities in Cloudborg. Filled with huge shops and villages and a strong millitary that houses inside the castle. Here you see many places that to explore. You can go to the Inn where many adventurers find their purpose and spirit. The Castle also has a great training grounds and equipment for new adventurers. There's also some exciting Cloud Mountains to the left. Or you can travel to the ground. \n -Go to Inn \n -Go to Castle \n -Go to the Mountains \n -Travel to the ground").toLowerCase();
         
         if(city == "go to inn" || city == "inn"){
             Inn();
@@ -62,36 +72,65 @@ function Game(){
         }
         
         else{
-            alert("I don't understand doctor " + city);
+            alert("I don't understand " + city + ", you dummy!");
             City();
         }
                 
         function Inn(){
-            var inn = prompt("Well " + jeffName + " this is the place of heart and soul, the place every adventurer dreams of. Here folks of all types stay. It is a clean and huge Inn, made up of wood it almost looks like a huge cabin. In the corner you see a banner and members from the Adventurer Guild saying they're hiring. At the bar are you see the barkeeper and some guests are talking about politics. And Upstairs there is a bed you can sleep on \n -Talk to Guild \n -Chat with Barkeeper \n -Sleep");
+            var inn = prompt("Well " + jeffName + " this is the place of heart and soul, the place every adventurer dreams of. Here folks of all types stay. It is a clean and huge Inn, made up of wood it almost looks like a huge cabin. In the corner you see a banner and members from the Adventurer Guild saying they're hiring. At the bar are you see the barkeeper and some guests are talking about politics. And Upstairs there is a bed you can sleep on \n -Talk to Guild \n -Chat with Barkeeper \n -Sleep").toLowerCase();
+            
+            if(inn == "talk to guild" || inn == "guild"){
+                
+            }
+            
+            else if(inn == "chat with barkeeper" || inn == "barkeeper"){
+                
+            }
+            
+            else if(inn == "sleep"){
+                if inventory.coins > 0{
+                    inventory.coins = 0
+                    alert("While you were sleeping last night a thief came and stole all your money")
+                    Inn()
+                }
+                else{
+                    alert("While you were sleeping last night a thief came and attempted to steal your money, but because you suck you had no money to steal")
+                    Inn()
+                }
+            }
         }
         
         function Castle(){
-            var castleEntrance = prompt("Impressive isn't " + jeffName + " you stand at a huge gate you can see 20 or so towers. Here you see this massive fortress made out of red and black brick. You approach the gate two guards are there holding spears looking at you menacingly. One of the guards yell at you to state your business. \n -Run Away \n -None of your business \n -Here to Loot \n -I Need to See the King \n -Here to train");
+            var castleEntrance = prompt("Impressive isn't " + jeffName + " you stand at a huge gate you can see 20 or so towers in the distance. Here you see this massive fortress made out of red and black brick. You approach the gate two guards are there holding spears looking at you menacingly. One of the guards yell at you to state your business. \n -Run Away \n -None of your business \n -Here to Loot \n -I Need to See the King \n -Here to train").toLowerCase();
             
             if(castleEntrance == "run away" || castleEntrance == "run"){
                 City();
             }
             
-            if(castleEntrance == "none of your business" || castleEntrance == "business"){
+            else if(castleEntrance == "none of your business" || castleEntrance == "business"){
                 alert("The guard yells at you telling you that scumbags like you have no business here")
                 Castle();
             }
             
-            if(castleEntrance == "here to loot" || castleEntrance == "loot"){
+            else if(castleEntrance == "here to loot" || castleEntrance == "loot"){
                 alert("The guards thank you for admitting you're a theif and take you to the dungeon")
                 Dungeon();
             }
             
-            if(castleEntrance == "i need to see the king" || castleEntrance == "see the king" || castleEntrance == "king"){
+            else if(castleEntrance == "i need to see the king" || castleEntrance == "see the king" || castleEntrance == "king"){
                 alert("The guards call you stupid as there is no king")
                 Castle();
             }
+            
+            else if(castleEntrance == "here to train" || castleEntrance == "train"){
+                alert("The guards open the gate and one of the guards escort you to the barracks")
+                Barracks();
+            }
+            
+            else{
+                alert("I don't understand " + castleEntrance + ", you dummy!");
+                Castle();
+            }
         }
+        }  
     }
-}
-    
